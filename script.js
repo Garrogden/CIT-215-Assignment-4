6285851c3084d74ca8e06a7f320f132fe5a7d7b6
Garret O <67571983+Garrogden@users.noreply.github.com>
2020-09-21 18:01:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,6 @@ var chrs = [
   { name: "Bumblebee", class: "scout", afl: "autobot", vehicle: "car" },
   { name: "Starscream", class: "scout", afl: "decepticon", vehicle: "plane" },
   { name: "Ironhide", class: "soldier", afl: "autobot", vehicle: "truck" },
-  { name: "Brawl", class: "soldier", afl: "decepticon", vehicle: "tank" },
-];
-
-var chrs = [
-  { name: "Optimus Prime", class: "leader", afl: "autobot", vehicle: "truck" },
-  { name: "Megatron", class: "leader", afl: "decepticon", vehicle: "tank" },
-  { name: "Bumblebee", class: "scout", afl: "autobot", vehicle: "car" },
-  { name: "Starscream", class: "scout", afl: "decepticon", vehicle: "plane" },
-  { name: "Ironhide", class: "soldier", afl: "autobot", vehicle: "truck" },
   { name: "Brawl", class: "soldier", afl: "decepticon", vehicle: "tank" }
 ];
 
